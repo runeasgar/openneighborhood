7237418359393afe4cb8270054924c09f1c67628
root <root@linode.joshuataylor.co>
2013-06-26 00:43:58 +0000

    setting up env var for memcache and database include. removed settings.php from gitignore.

---
+++ sites/default/database.php
@@ -0,0 +1,15 @@
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'on_dev',
+      'username' => 'drupal',
+      'password' => 'Linode#13mysql-drupal',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
