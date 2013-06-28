2c21a47de40a3c55b7b3f9d4f1cb1932276dce86
root <root@linode.joshuataylor.co>
2013-06-28 19:05:42 +0000

    continuing directory structure changes

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -210,24 +210,6 @@
  *   );
  * @endcode
  */
-/* 
-$databases = array (
-  'default' => 
-  array (
-    'default' => 
-    array (
-      'database' => 'on_dev',
-      'username' => 'drupal',
-      'password' => 'Linode#13mysql-drupal',
-      'host' => 'localhost',
-      'port' => '',
-      'driver' => 'mysql',
-      'prefix' => '',
-    ),
-  ),
-); */
-
-//require_once('database.php');
 
 /**
  * Access control for update.php script.
